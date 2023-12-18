c2ac1102d37a564693f9e33a03c7c1ee53d19db9
alx_27 <didieralejandromejia83@gmail.com>
2023-12-18 15:09:28 -0500

    send emails success

--- routers/email.py
+++ routers/email.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import os
+from dotenv import load_dotenv
 
 root = APIRouter(prefix="/sendEmail",
                  tags=["sendEmail"],
@@ -10,4 +12,27 @@ root = APIRouter(prefix="/sendEmail",
 
 @root.post("/", response_model=dict)
 async def send_email(email_data: dict):
-    return email_data
+    load_dotenv()
+        
+    msg = MIMEMultipart()
+    msg['From'] = os.getenv('USER_EMAIL')
+    msg['To'] = email_data["email"]
+    msg['Subject'] = "Thanks for use my program"
+    
+    tMessage = f'Hola mucho gusto'
+    
+    email = MIMEText(tMessage,"plain")
+    
+    msg.attach(email)
+    
+    mailServer = smtplib.SMTP(os.getenv('SMTP_SSL'), 587)
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(os.getenv('USER_EMAIL'), os.getenv('PASSWORD_EMAIL'))
+    
+    mailServer.sendmail(os.getenv('USER_EMAIL'), email_data["email"], msg.as_string())
+    
+    mailServer.close()
+    
+    return {"message": "email send"}
